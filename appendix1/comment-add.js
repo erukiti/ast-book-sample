9f1d4165aa82899e69563b2526d6d00e8f3629f1
erukiti <erukiti@gmail.com>
2017-11-04 11:17:08 +0900

    コメント関連のサンプルを追加・編集

---
+++ appendix1/comment-add.js
@@ -0,0 +1,21 @@
+const {transform} = require('babel-core')
+
+const src = `hoge()`
+
+const plugin = {
+  visitor: {
+    CallExpression: (nodePath, state) => {
+      nodePath.addComment('leading', '*\n * hoge\n * fuga\n * piyo\n *')
+      nodePath.addComments('trailing', [
+        {type: 'CommentLine', value: '\n\n\n'},
+        {type: 'CommentLine', value: '1'},
+        {type: 'CommentLine', value: '2'},
+        {type: 'CommentLine', value: '3'},
+        {type: 'CommentLine', value: '4'},
+      ])
+    },
+  },
+}
+
+const {code} = transform(src, {plugins: [plugin]})
+console.log(code)
---
+++ appendix1/comment-viewer.js
@@ -0,0 +1,26 @@
+const {transform} = require('babel-core')
+
+// const src = ......
+
+const plugin = {
+  visitor: {
+    Program: (nodePath, state) => {
+      nodePath.traverse({
+        enter: (innerPath) => {
+          const {leadingComments, trailingComments, type} = innerPath.node
+          if (leadingComments || trailingComments) {
+            console.log(`${type}: ${innerPath.getSource()}`)
+            if (leadingComments) {
+              console.log(`  leading:  ${leadingComments.map(n => n.value).join(', ')}`)
+            }
+            if (trailingComments) {
+              console.log(`  trailing: ${trailingComments.map(n => n.value).join(', ')}`)
+            }
+          }
+        }
+      })
+    }
+  }
+}
+
+transform(src, {plugins: [plugin]})
